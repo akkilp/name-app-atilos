33892cdf9cafe6eea8abaa262de820f25145eb8b
Aleksi Kilponen <akkilp@utu.fi>
2021-01-15 16:38:34 +0200

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from './app';
 import NameController from './names/name.controller';
 
+import 'dotenv/config';
+
 const PORT = process.env.PORT || 5000;
 
 // Initialize server with App Class
