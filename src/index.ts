fe016b53f40433785840da36232c64a84035e267
Aleksi Kilponen <akkilp@utu.fi>
2021-01-20 16:47:23 +0200

    createDBconnection utility function, configuration for conditional db configs

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,26 @@
 /* eslint-disable func-names */
 import * as dotenv from 'dotenv';
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
 
 import App from './app';
 import NameController from './names/name.controller';
 
-/* TypeORM and process.env configuration */
-import config from './ormconfig';
-
+import createDBConnection from './utils/createDBConnection';
 import validateEnv from './utils/validateEnv';
 
 dotenv.config({ path: `${__dirname}/src/.env` });
 
 const PORT = process.env.PORT || 5000;
 
+// Validate environmental variables
 validateEnv();
 
-(async function () {
-  try {
-    await createConnection(config);
-  } catch (error) {
-    console.log('Error while connecting to the database\n', error);
-    return error;
-  }
+const startServer = async () => {
+  await createDBConnection();
 
   // Initialize server with App Class
   const app = new App([new NameController()], PORT as number);
   app.listen();
-})();
+};
+
+startServer();
